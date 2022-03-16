01ce361d41307003db1e698518fb8ae1b629397d
acastano <78517677+acastano90931@users.noreply.github.com>
2022-03-15 19:51:01 -0500

    Add files via upload

---
+++ numeroInteligente.cs
@@ -0,0 +1,31 @@
+public class numeroInteligente (List<int> num){
+
+int sum = 0;
+
+        for (int i = 0; i < num.Count; i++)
+        {
+            sum += num[i];
+        }
+        return sum;
+
+   if (sum > 0 ){
+
+       console.WriteLine("El número es positivo");
+
+   }
+           
+       
+    else 
+   {
+     console.WriteLine("El número es negativo");
+   }
+
+}
+
+
+public static void Main()
+{
+
+console.WriteLine("Los numeros a validar son los siguientes:" + num);
+
+}
